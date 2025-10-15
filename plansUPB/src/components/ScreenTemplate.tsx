81381bc13b7befa783a77d877313eec7eda57350
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-10-14 23:35:27 -0400

    feat[corrections]: update ScreenTemplate, made ScrollView optional and remove titles

--- plansUPB/src/components/ScreenTemplate.tsx
+++ plansUPB/src/components/ScreenTemplate.tsx
@@ -1,17 +1,15 @@
 import React, { ReactNode } from 'react';
 import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
-import { Text } from '@ui-kitten/components';
 import { globalStyles } from '../styles/globals';
 import { useThemeColors } from '../hooks/useThemeColors';
 
 interface TemplateProps {
-    title: string;
-    subtitle: string;
     children: ReactNode;
+    omitScroll?: boolean;
     floatingButton?: ReactNode;
 }
 
-function ScreenTemplate({ title, subtitle, children, floatingButton }: TemplateProps) {
+function ScreenTemplate({ children, omitScroll = false, floatingButton }: TemplateProps) {
     const { theme } = useThemeColors();
 
     const barStyle = theme === 'dark' ? 'light-content' : 'dark-content';
@@ -19,22 +17,21 @@ function ScreenTemplate({ title, subtitle, children, floatingButton }: TemplateP
     return (
         <SafeAreaView style={globalStyles().safeArea}>
             <StatusBar barStyle={barStyle} />
-            <ScrollView
-                style={{ flex: 1 }}
-                contentContainerStyle={globalStyles().app_scroll}
-                bounces={true}
-                keyboardShouldPersistTaps="handled"
-            >
+            {omitScroll ? (
                 <>
-                    <Text category="h4" style={{ marginBottom: 8 }}>
-                        {title}
-                    </Text>
-                    <Text style={[globalStyles().app_subtitle, { marginBottom: 16 }]}>
-                        {subtitle}
-                    </Text>
                     {children}
                 </>
-            </ScrollView>
+            ) : (
+                <ScrollView
+                    style={{ flex: 1 }}
+                    contentContainerStyle={globalStyles().app_scroll}
+                    bounces={true}
+                    keyboardShouldPersistTaps="handled"
+                >
+                    {children}
+                </ScrollView>
+            )}
+
             {floatingButton}
         </SafeAreaView>
     );
