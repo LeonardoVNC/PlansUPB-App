9f6b77fdf44ceafba72c19bd0d989b0936451cc0
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-10-22 01:03:57 -0400

    feat[plans]: add maps to load UI info for categories and status

---
+++ plansUPB/src/styles/planCategoryMap.ts
@@ -0,0 +1,7 @@
+export const iconCategoryMap: Map<string, string> = new Map ([
+    ['Comida', "pizza-outline"],
+    ['Cine', 'film-outline'],
+    ['Juegos', 'game-controller-outline'],
+    ['Estudio', 'book-outline'],
+    ['Otro', 'people-outline']
+])
---
+++ plansUPB/src/styles/planStatusMap.ts
@@ -0,0 +1,13 @@
+export const cardStatusMap: Map<string, string> = new Map([
+    ['draft', 'warning'],
+    ['open', 'primary'],
+    ['closed', 'success'],
+    ['cancelled', 'danger']
+])
+
+export const iconStatusMap: Map<string, string> = new Map ([
+    ['draft', 'clipboard-outline'],
+    ['open', 'clock-outline'],
+    ['closed', 'checkmark-circle-2-outline'],
+    ['cancelled', 'close-outline']
+])
